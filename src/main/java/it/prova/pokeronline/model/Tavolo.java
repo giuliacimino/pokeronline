95f16c1d7d1c23d979c3c4715001ffa1122f3df0
giuliacimino <giuliacimino00@gmail.com>
2023-05-23 12:34:57 +0200

    creazione tabella sql

---
+++ src/main/java/it/prova/pokeronline/model/Tavolo.java
@@ -0,0 +1,159 @@
+package it.prova.pokeronline.model;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+
+
+@Entity
+@Table(name = "tavolo")
+public class Tavolo {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	
+	@Column(name = "esperienzaMin")
+	private Integer esperienzaMin;
+	
+	@Column(name = "cifraMin")
+	private Integer cifraMin;
+	
+	@Column(name = "denominazione")
+	private String denominazione;
+	
+	@Column(name = "dataCreazione")
+	private LocalDate dataCreazione;
+	
+	
+	@OneToMany(fetch = FetchType.LAZY)
+	private Set<Utente> giocatori= new HashSet<>(0);
+	
+	
+	@OneToOne(fetch = FetchType.LAZY)
+	private Utente utenteCreatore;
+
+
+	public Tavolo() {
+		super();
+	}
+
+
+	public Tavolo(Long id, Integer esperienzaMin, Integer cifraMin, String denominazione, LocalDate dataCreazione,
+			Set<Utente> giocatori, Utente utenteCreatore) {
+		super();
+		this.id = id;
+		this.esperienzaMin = esperienzaMin;
+		this.cifraMin = cifraMin;
+		this.denominazione = denominazione;
+		this.dataCreazione = dataCreazione;
+		this.giocatori = giocatori;
+		this.utenteCreatore = utenteCreatore;
+	}
+
+
+	public Tavolo(Integer esperienzaMin, Integer cifraMin, String denominazione, LocalDate dataCreazione,
+			Set<Utente> giocatori, Utente utenteCreatore) {
+		super();
+		this.esperienzaMin = esperienzaMin;
+		this.cifraMin = cifraMin;
+		this.denominazione = denominazione;
+		this.dataCreazione = dataCreazione;
+		this.giocatori = giocatori;
+		this.utenteCreatore = utenteCreatore;
+	}
+
+
+	public Tavolo(Integer esperienzaMin, Integer cifraMin, String denominazione, LocalDate dataCreazione) {
+		super();
+		this.esperienzaMin = esperienzaMin;
+		this.cifraMin = cifraMin;
+		this.denominazione = denominazione;
+		this.dataCreazione = dataCreazione;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public Integer getEsperienzaMin() {
+		return esperienzaMin;
+	}
+
+
+	public void setEsperienzaMin(Integer esperienzaMin) {
+		this.esperienzaMin = esperienzaMin;
+	}
+
+
+	public Integer getCifraMin() {
+		return cifraMin;
+	}
+
+
+	public void setCifraMin(Integer cifraMin) {
+		this.cifraMin = cifraMin;
+	}
+
+
+	public String getDenominazione() {
+		return denominazione;
+	}
+
+
+	public void setDenominazione(String denominazione) {
+		this.denominazione = denominazione;
+	}
+
+
+	public LocalDate getDataCreazione() {
+		return dataCreazione;
+	}
+
+
+	public void setDataCreazione(LocalDate dataCreazione) {
+		this.dataCreazione = dataCreazione;
+	}
+
+
+	public Set<Utente> getGiocatori() {
+		return giocatori;
+	}
+
+
+	public void setGiocatori(Set<Utente> giocatori) {
+		this.giocatori = giocatori;
+	}
+
+
+	public Utente getUtenteCreatore() {
+		return utenteCreatore;
+	}
+
+
+	public void setUtenteCreatore(Utente utenteCreatore) {
+		this.utenteCreatore = utenteCreatore;
+	}
+	
+	
+
+}
---
+++ src/main/java/it/prova/pokeronline/model/Utente.java
@@ -0,0 +1,138 @@
+package it.prova.pokeronline.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "utente")
+public class Utente {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	
+	@Column(name = "nome")
+	private String nome;
+	
+	@Column(name = "cognome")
+	private String cognome;
+	
+	@Column(name = "username")
+	private String username;
+	
+	@Column(name = "password")
+	private String password;
+	
+	@Column(name = "dataRegistrazione")
+	private LocalDate dataRegistrazione;
+	
+	@Column(name = "stato")
+	private StatoUtente stato;
+	
+	@Column(name = "creditoAccumulato")
+	private Integer creditoAccumulato;
+
+	public Utente() {
+		super();
+	}
+
+	public Utente(Long id, String nome, String cognome, String username, String password, LocalDate dataRegistrazione,
+			StatoUtente stato, Integer creditoAccumulato) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.cognome = cognome;
+		this.username = username;
+		this.password = password;
+		this.dataRegistrazione = dataRegistrazione;
+		this.stato = stato;
+		this.creditoAccumulato = creditoAccumulato;
+	}
+
+	public Utente(String nome, String cognome, String username, String password, LocalDate dataRegistrazione,
+			StatoUtente stato, Integer creditoAccumulato) {
+		super();
+		this.nome = nome;
+		this.cognome = cognome;
+		this.username = username;
+		this.password = password;
+		this.dataRegistrazione = dataRegistrazione;
+		this.stato = stato;
+		this.creditoAccumulato = creditoAccumulato;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public LocalDate getDataRegistrazione() {
+		return dataRegistrazione;
+	}
+
+	public void setDataRegistrazione(LocalDate dataRegistrazione) {
+		this.dataRegistrazione = dataRegistrazione;
+	}
+
+	public StatoUtente getStato() {
+		return stato;
+	}
+
+	public void setStato(StatoUtente stato) {
+		this.stato = stato;
+	}
+
+	public Integer getCreditoAccumulato() {
+		return creditoAccumulato;
+	}
+
+	public void setCreditoAccumulato(Integer creditoAccumulato) {
+		this.creditoAccumulato = creditoAccumulato;
+	}
+	
+	
+	
+
+}
